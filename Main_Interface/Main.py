e5016d7ee1f8bc942804d99c19babd70f2db442c
Tom Nguyen <huuthong103@gmail.com>
2017-10-02 13:10:41 -0700

    sample python

--- Main_Interface/Main.py
+++ Main_Interface/Main.py
@@ -1,3 +1,9 @@
-from Import_Export import Export_File
-class Main():
-    CSV=Export_File.exportCSV().exportToFile()
+# Store input numbers
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+# Add two numbers
+sum = float(num1) + float(num2)
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
