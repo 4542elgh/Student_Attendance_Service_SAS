27e12ab8176ab7c683fd3d0f65020f2db10a4f1d
4542elgh <4542elgh@gmail.com>
2017-10-05 22:09:50 -0700

    add fully functional PBKDF2 hashing algorithm with SHA256 HMAC

--- Hashing_PBKDF2/PBKDF2_Algorithm.py
+++ Hashing_PBKDF2/PBKDF2_Algorithm.py
@@ -1,10 +1,33 @@
-from pbkdf2 import PBKDF2
-from Crypto.Cipher import AES
+from passlib.hash import pbkdf2_sha256
+import pickle
 import os
 
+
+
 class PBKDF2_Algorithm():
-    salt = os.urandom(8)  # 64-bit salt
-    key = PBKDF2("This passphrase is a secret.", salt).read(32)  # 256-bit key
-    print(key)
-    iv = os.urandom(16)  # 128-bit IV
-    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    def generate_Salt(self): #generate an 8-bit salt for hashing password
+        return os.urandom(8)
+
+    def generate_Hash(self,username,inputPassword):
+        salt = PBKDF2_Algorithm.generate_Salt(object) #calling within the same class require class name.def
+        hash=pbkdf2_sha256.encrypt(inputPassword,rounds=10000,salt=salt) #with raw password, iterate it with salt 10000 times to avoid brute force and dictionary attack or rainbow attack
+        PBKDF2_Algorithm.storing_Salt_Hash(object,username,salt,hash) #store username,salt,hash, BUT NOT RAW PASSWORD
+
+    def storing_Salt_Hash(self, username, salt, hash):
+        try: #try if the file exist or not
+            with open('../Hashing_PBKDF2/Admin_Login.pickle','rb') as readFile:  # load pickle file and load it into variable 'temp'
+                temp = pickle.load(readFile)
+                temp[username] = [salt, hash]  # add a new entry for specific user with specific salt and hash
+            with open('../Hashing_PBKDF2/Admin_Login.pickle', 'wb') as writeFile:
+                pickle.dump(temp, writeFile, protocol=pickle.HIGHEST_PROTOCOL) # write it to file
+        except IOError: #if the file doesnt exist
+            with open('../Hashing_PBKDF2/Admin_Login.pickle', 'wb') as writeFile: #empty file
+                temp={} #empty dict to store first input value
+                temp[username]=[salt,hash]
+                pickle.dump(temp, writeFile, protocol=pickle.HIGHEST_PROTOCOL) #write to file
+
+    def check_Password(self,username,password):
+        with open('../Hashing_PBKDF2/Admin_Login.pickle', 'rb') as readFile:
+            passwordList = pickle.load(readFile) #fetch all the dict entry from file
+            print(pbkdf2_sha256.verify(password,passwordList[username][1])) # get the user input password (login) and check it with the hash from that user's dict, python will keep track which algorithm u use and how many iteration you used.
--- Main_Interface/Main.py
+++ Main_Interface/Main.py
@@ -1,3 +1,7 @@
 from Hashing_PBKDF2 import PBKDF2_Algorithm
 class Main():
-    PBKDF2_Algorithm.PBKDF2_Algorithm
+    PBKDF2_Algorithm.PBKDF2_Algorithm.generate_Hash(object,"evan","easypassword")
+    a=[1,2,3,4,5,6,7,8,9,0]
+    for element in a:
+        print(element)
+    PBKDF2_Algorithm.PBKDF2_Algorithm.check_Password(object,"evan","anotherpassword")
