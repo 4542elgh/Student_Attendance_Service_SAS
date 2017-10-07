7b9f994be78a078b93bb156c4ff764941e3655ed
fjason2011 <fjason2011@yahoo.com>
2017-10-07 14:24:13 -0700

    export-usb-data

--- Import_Export/Export_Abstract.py
+++
@@ -1,7 +0,0 @@
-from abc import ABCMeta,abstractmethod
-
-class Export_Abstract:
-    
-    @abstractmethod
-    def exportToFile(self):
-        pass
--- Main_Interface/Main.py
+++
@@ -1,3 +0,0 @@
-from Import_Export import Export_File
-class Main():
-    CSV=Export_File.exportCSV().exportToFile()
