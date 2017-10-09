a6846828c8fa5abe9c4ad9b127272e3abf0f5b12
imcano <imcano97@gmail.com>
2017-10-09 00:42:00 -0700

    Added Load Roster, Admin login links to it, .png and .gif files for testing purposes

--- User_Interface/Admin_Login.py
+++ User_Interface/Admin_Login.py
@@ -2,20 +2,24 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QLineEdit, QPushButton
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import QUrl, pyqtSlot
+from Load_Roster import LoadRoster
 
 
 class AdminLogin(QMainWindow):
-    def __init__(self):
+    def __init__(self, parent=None):
         super().__init__()
         self.title = 'Administrator Login'
+
         self.left = 50
         self.top = 50
         self.width = 300
         self.height = 480
-        self.initUI()
-        self.w = None
 
-    def initUI(self):
+        self.init_ui()
+
+        self.newWindow = LoadRoster(self)
+
+    def init_ui(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
@@ -40,16 +44,22 @@ class AdminLogin(QMainWindow):
         login_button = QPushButton('Login', self)
         login_button.setToolTip('Login')
         login_button.move(175, 420)
-        login_button.clicked.connect(self.on_login_button_clicked)
+        login_button.clicked.connect(self.open_window)
 
         alternate_login = QLabel('Alternate Login', self)
         alternate_login.setText('''<a href='smileman.gif'>Alternate Login</a>''')
         alternate_login.setOpenExternalLinks(True)
         alternate_login.move(200, 450)
 
+        self.show()
+
+    @pyqtSlot()
+    def open_window(self):
+        admin.close()
+        self.newWindow.show()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     admin = AdminLogin()
-    admin.show()
     sys.exit(app.exec_())
