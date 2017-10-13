cca1498a9d826bc852331d934563d625d322397a
imcano <imcano97@gmail.com>
2017-10-12 23:38:43 -0700

    Load_Roster textbar gets updated when user selects a file

--- User_Interface/Load_Roster.py
+++ User_Interface/Load_Roster.py
@@ -1,14 +1,15 @@
 import sys
+import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit, QFileDialog, QLabel, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
 
 
 class LoadRoster(QWidget):
+
     def __init__(self, parent=None):
         super().__init__()
         self.title = 'Load Roster'
-
         self.left = 200
         self.top = 200
         self.width = 600
@@ -21,7 +22,9 @@ class LoadRoster(QWidget):
 
         label_filename = QLabel('Path:', self)
         label_filename.move(65, 305)
+
         textbox_filename = QLineEdit(self)
+        textbox_filename.setObjectName("line")
         textbox_filename.resize(400, 20)
         textbox_filename.move(100, 300)
 
@@ -30,7 +33,7 @@ class LoadRoster(QWidget):
         button_file.setIconSize(QSize(20, 20))
         button_file.setMaximumSize(25, 22.5)
         button_file.move(505, 299)
-        button_file.clicked.connect(self.openFileNamesDialog)
+        button_file.clicked.connect(self.open_file_names_dialog)
 
         button_submit = QPushButton('', self)
         button_submit.setIcon(QIcon('go.png'))
@@ -38,13 +41,17 @@ class LoadRoster(QWidget):
         button_submit.setMaximumSize(25, 22.5)
         button_submit.move(535, 299)
 
-    def openFileNamesDialog(self):
+    def open_file_names_dialog(self):
+
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         files, _ = QFileDialog.getOpenFileNames(self, "QFileDialog.getOpenFileNames()", "",
                                                 "All Files (*);;Python Files (*.py)", options=options)
         if files:
-            print(files)
+            file_name = files[0]
+
+            line = self.findChild(QLineEdit, "line")
+            line.setText(file_name)
 
 
 if __name__ == '__main__':
