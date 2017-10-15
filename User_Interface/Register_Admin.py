3358e524070510399f8a97b756e9d2b689edb690
4542elgh <4542elgh@gmail.com>
2017-10-14 17:12:31 -0700

    fix error message for registering new account

--- User_Interface/Register_Admin.py
+++ User_Interface/Register_Admin.py
@@ -5,7 +5,6 @@ from PyQt5.QtCore import QUrl, pyqtSlot
 from Hashing_PBKDF2 import PBKDF2_Algorithm
 from User_Interface import Admin_Login
 
-
 class Register_Admin(QMainWindow):
     def __init__(self, parent=None):
         super().__init__()
@@ -16,6 +15,7 @@ class Register_Admin(QMainWindow):
         self.height = 550
         self.hash=PBKDF2_Algorithm.PBKDF2_Algorithm
         self.init_ui()
+        self.newWindow = Admin_Login
 
     def init_ui(self):
         self.setWindowTitle(self.title)
@@ -23,8 +23,8 @@ class Register_Admin(QMainWindow):
 
         self.label_login_error = QLabel("login error,\ncheck your credentials", self)
         self.label_login_error.setStyleSheet("color:red")
-        self.label_login_error.resize(300,40)
-        self.label_login_error.move(40, 415)
+        self.label_login_error.resize(300,80)
+        self.label_login_error.move(25, 455)
         self.label_login_error.hide()
 
         label_logo = QLabel(self)
@@ -71,58 +71,22 @@ class Register_Admin(QMainWindow):
             if(self.hash.check_Identical_Password(self,self.textbox_password.text(),self.textbox_re_password.text())):
                 if(self.hash.check_Password_Length(self,self.textbox_password.text())):
                     if(self.hash.check_Special_Character(self.textbox_password.text())):
-                        # show_Exit_Dialog(self)
-                        msg = QMessageBox(self)
-                        msg.setIcon(QMessageBox.Question)
-                        msg.setWindowTitle('Confirmation')
-                        msg.setText('Register Sucess!:')
-                        another_admin = msg.addButton('Register Another Admin', QMessageBox.AcceptRole)
-                        return_SignIn = msg.addButton('Return to Sign-in', QMessageBox.AcceptRole)
-                        quit = msg.addButton('Exit Program', QMessageBox.RejectRole)
-                        msg.setDefaultButton(return_SignIn)
-                        msg.exec_()
-                        msg.deleteLater()
-
-                        if msg.clickedButton() is another_admin:
-                            self.restart()
-                        elif msg.clickedButton() is return_SignIn:
-                            self.newWindow = Admin_Login.AdminLogin(self)
-                            self.newWindow.show()
-                            register_admin.close()
-                        else:
-                            self.close()
+                        self.newWindow = Admin_Login.AdminLogin(self)
+                        self.newWindow.show()
+                        register_admin.close()
                     else:
-                        print("Please make sure your password have 1 upper 1 lower 1 number 1 special character")
+                        self.label_login_error.setText("Please make sure your password\nhave 1 upper 1 lower\n1 number 1 special character")
+                        self.label_login_error.show()
                 else:
-                    print("passwords length need to be between 6 to 16 characters")
+                    self.label_login_error.setText("passwords length need\nto be between 6 to 16\ncharacters")
+                    self.label_login_error.show()
             else:
-                print("passwords is not identical")
+                self.label_login_error.setText("passwords is not identical")
                 self.label_login_error.show()
 
         else:
-            print("username is taken")
+            self.label_login_error.setText("username is taken")
             self.label_login_error.show()
-
-    # def show_Exit_Dialog(self):
-    #     msg = QMessageBox(self)
-    #     msg.setIcon(QMessageBox.Question)
-    #     msg.setWindowTitle('Confirmation')
-    #     msg.setText('Register Sucess!:')
-    #     another_admin = msg.addButton('Register Another Admin', QMessageBox.AcceptRole)
-    #     return_SignIn = msg.addButton('Return to Sign-in', QMessageBox.AcceptRole)
-    #     quit = msg.addButton('Exit Program', QMessageBox.RejectRole)
-    #     msg.setDefaultButton(return_SignIn)
-    #     msg.exec_()
-    #     msg.deleteLater()
-    #
-    #     if msg.clickedButton() is another_admin:
-    #         print('RESTART')
-    #         self.restart()
-    #     elif msg.clickedButton() is return_SignIn:
-    #         self.newWindow.show()
-    #         register_admin.close()
-    #     else:
-    #         self.close()
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     register_admin = Register_Admin()
