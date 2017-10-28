92164dcd3197f0a9ef219c24303ca468ebaf5a0f
fjason2011 <fjason2011@yahoo.com>
2017-10-28 14:29:49 -0700

    usb

---
+++ Import_Export/Import_File.py
@@ -0,0 +1,25 @@
+from Import_Export import Import_Abstract
+
+class importXML(Import_Abstract.Import_Abstract):
+    def importParser(self):
+        #implement xml parser in here
+        pass
+    def toList(self):
+        pass
+        #implement object to list here
+        
+class importCSV(Import_Abstract.Import_Abstract):
+    def importParser(self):
+        #implement CSV parser in here
+        pass
+    def toList(self):
+        pass
+        #implement object to list here
+
+class importJSON(Import_Abstract.Import_Abstract):
+    def importParser(self):
+        #implement JSON parser in here
+        pass
+    def toList(self):
+        pass
+        #implement object to list here
