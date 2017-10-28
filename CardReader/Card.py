e4b526382279a66fee0ad3eebced4bc4e2a99174
Tom Nguyen <huuthong103@gmail.com>
2017-10-28 16:24:23 -0700

    updated card reader

--- CardReader/Card.py
+++ CardReader/Card.py
@@ -8,11 +8,18 @@ def paste_string():
 
     name = split[1]
 
+    # Split the slash inside the card reader
     forward_slash = split[1].find('/')
+    # Get first name from the name
     first_name = name[forward_slash + 1:len(name)]
+    # Get last name from the name
     last_name = name[0:forward_slash]
     three = split[2]
     cin = three[len(three) - 10:len(three) - 1]
+
+
+    # Print out the first name, last name, and CIN number
+
     print("First Name: " + first_name + " Last Name: " + last_name + " CIN: " + cin)
 
 
